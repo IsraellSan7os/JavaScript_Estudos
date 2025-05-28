317329d34d651b8b2cb39bc3ca1a7b049e0bbb6b
Israel Santos <ysraellsanosss@gmail.com>
2025-05-28 20:27:14 -0300

    Variaveis var

---
+++ variaveis_constantes/js/var.js
@@ -0,0 +1,19 @@
+// Var O valor de uma variavel pode ser alterado
+console.log("Estudos com var")
+
+var nome = "Israel"
+var idade = 22
+var peso = 70.5
+
+
+console.log("Nome: " + nome)
+console.log("Idade: " + idade )
+console.log("klg: " + peso)
+
+
+var numero = 10 
+var numero2 = 20
+var soma = numero + numero2
+console.log(soma)
+
+console.log("Fim")
