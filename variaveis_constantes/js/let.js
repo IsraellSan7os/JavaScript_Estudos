a43f08e25ba2fa65715f7370cf5f7cce1ddb6082
Israel Santos <ysraellsanosss@gmail.com>
2025-05-28 20:41:21 -0300

    Variável let

---
+++ variaveis_constantes/js/let.js
@@ -0,0 +1,15 @@
+// Let 
+// Declara uma variável sem valor
+
+let use = "Anne"
+console.log(use);
+
+// let use = "Ana"
+// Primeira consideração: let não permite que uma variável seja declarada mais de uma vez
+
+let email = "ana@gmail.com"
+console.log(imail); 
+
+email = "Joao@emaill.com"
+console.log(email);
+// Segunda consideração: let permite que uma variável seja redeclarada
