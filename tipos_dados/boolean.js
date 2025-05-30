9bb9be77cbbc0d01c2ca5d4c30978d4074f50ec3
Israel Santos <ysraellsanosss@gmail.com>
2025-05-30 05:29:04 -0300

    Boolean

---
+++ tipos_dados/boolean.js
@@ -0,0 +1,8 @@
+// Boolean 
+// So armazenam dois valores: true ou false
+
+console.log(typeof true);
+console.log(typeof false);
+
+let inLoading = true;
+console.log(`Esta carregando: ${inLoading}`);
