ea146a837114e4b742103bf824b76ff2238c9afb
Israel Santos <ysraellsanosss@gmail.com>
2025-05-30 05:02:34 -0300

    String formatação de texto

---
+++ tipos_dados/string.js
@@ -0,0 +1,20 @@
+// String 
+let username = "Israel"
+let number = 123
+
+console.log(username)
+console.log(number)
+
+console.log(typeof username)
+console.log(typeof number)
+
+// Diferença enter aspas "" e as aspas simples ''
+console.log("Ola, eu 'sou uma' string")
+console.log('Ola, eu "sou uma" string')  
+
+
+// crases 
+console.log(`Uma string com acentos grasves permite fazer uma formatação 
+            com varias linhas "Posso usar aspas duplas" e posso usar 
+            aspas simples 'Posso usar aspas simples'
+    `)
