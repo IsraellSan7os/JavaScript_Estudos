594ba8f8aca21292e15c7a144680a48b82726e07
Israel Santos <ysraellsanosss@gmail.com>
2025-05-30 05:25:19 -0300

    Number

---
+++ tipos_dados/number.js
@@ -0,0 +1,14 @@
+// Number
+// Interiros
+console.log( typeof 5);
+
+// Inteiros
+console.log(`Inteiro 5`);
+
+
+// interiro negativo
+console.log(`Inteiro -5`);
+
+// Numero reias e float
+console.log(`125.78`)
+
