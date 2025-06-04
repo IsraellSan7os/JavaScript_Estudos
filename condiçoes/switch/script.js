430ec86762feb68f66af83fece6560e598ddb07f
Israel Santos <ysraellsanosss@gmail.com>
2025-06-04 12:27:05 -0300

    Switch

---
+++ condiçoes/switch/script.js
@@ -0,0 +1,26 @@
+let option = 7
+
+switch (option) {
+    case 0:
+        console.log("Domingo")
+    case 1:
+        console.log("Segunda")
+        break;
+    case 2:
+        console.log("Terça")
+        break
+    case 3:
+        console.log("Quarta")
+        break
+    case 4:
+        console.log("Quinta")
+        break
+    case 5:
+        console.log("Sexta")
+        break
+    case 6:
+        console.log("Sabado")
+        break
+    default:
+        console.log("Opção inválida")
+}
