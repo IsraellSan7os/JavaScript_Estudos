141bb89f9cf31d5935126b8c08fb8327b3692b34
Israel Santos <ysraellsanosss@gmail.com>
2025-06-06 17:46:17 -0300

    Inicio dos estudos com DOM

---
+++ DOM/template/scripts.js
@@ -0,0 +1,14 @@
+// Visualiza o conteudo do documento.
+// Console.log(document)
+
+
+// Otendo o title sa página
+console.log(document.title)
+
+
+// Acessar as propriedades  pelo ID
+const guest = document.getElementById("guest-1")
+console.log(guest)
+
+// Mostra as propriedades do objeto.
+console.dir(guest)
