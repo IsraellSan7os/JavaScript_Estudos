75bcef1148886dcca0ee09eecf30076beb898dd0
Israel Santos <ysraellsanosss@gmail.com>
2025-07-18 17:30:20 -0300

    funções

--- revião-javaScript/funcoes/function.js
+++ revião-javaScript/funcoes/function.js
@@ -38,12 +38,12 @@ function calculo(a = "", b = "", c = "") {
     return res
 }
 
-let result = calculo(10, 30, 20 )
+let result = calculo(10, 30, 20)
 console.log("O valor usado é ", result)
 
 
 
-function numeroPar(numero){
+function numeroPar(numero) {
     var res = numero % 2
 
     if (res == 0) {
@@ -51,8 +51,61 @@ function numeroPar(numero){
     } else {
         return "É ímpar"
     }
-    
+
 }
 
 let resultado = numeroPar(11)
 console.log(resultado)
+
+
+
+
+function carros(nomeCarro, modeloCarro, ano, placa, anoAtual) {
+    let tempoUso = anoAtual - ano;
+
+    if (tempoUso >= 20) {
+        return `O carro ${nomeCarro} modelo ${modeloCarro} de placa ${placa} tem ${tempoUso} anos de uso e é antigo.`;
+    } else {
+        return `O carro ${nomeCarro} modelo ${modeloCarro} de placa ${placa} tem ${tempoUso} anos de uso e ainda é novo.`;
+    }
+}
+
+const resultadoCarro = carros("Corolla", "RLX", 2000, "1234-00AC", 2025);
+
+console.log("Informações do carro:", resultadoCarro);
+
+
+
+function showMessage(num, num1){
+
+    let n = num + num1
+
+    return `A soma entre ${num}  e ${num1} e ${n}`
+}
+
+let resposta = showMessage(10,23)
+console.log(resposta)
+
+
+function semana(saldoConta, Pagamento, fatura, deposito, saque){
+
+    let extratoConta = saldoConta - fatura
+       
+    console.log`Pagamento realizado ${extratoConta}`
+    
+    if (saque > saldoConta ) {
+        return `Valor insuficiente ${saldoConta} disponivel`
+    } else {
+        return `Saque realizado com suscesso Saldo atual de ${saldoConta}`
+    }
+    
+
+    
+  
+        
+        
+    
+}
+
+let resultadoSema = semana("4000", "3000", "200", "150", "400")
+console.log(resultadoSema)
